892ed081d2b06321d146c87a74a04c5aceef19ab
stdimitrov05 <stdimitrov05>
2022-07-09 23:45:34 +0300

    Node js register/login system

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-
 // Imports
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 9000;
 
 // Static Files
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 // Set View's
-app.set('views', './views');
-app.set('view engine', 'ejs');
+app.set("views", "views");
+app.set("view engine", "ejs");
 
 // Navigation
-app.get('', (req, res) => {
-    res.render('index',{text : 'Hello stdimitrov05 👨‍💻'})
-})
-
-app.get('/contact', (req, res) => {
-   res.sendFile(__dirname + '/views/contact.html')
-})
+app.get("", (req, res) => {
+  res.render("index", { text: "Hello stdimitrov05 👨‍💻" });
+});
 
 
-app.listen(port, () => console.info(`App listening on port ${port}`))
+app.get("/signup.html", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/signup.html"));
+});
+
+app.listen(port, () => console.info(`App listening on port ${port}`));
---
+++ public/js/system/confing.js
@@ -0,0 +1,58 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNmnZgQRcH0SWyksU_751yO0GUnbiFY-Y",
+
+  authDomain: "discordst-20a88.firebaseapp.com",
+
+  databaseURL: "https://discordst-20a88-default-rtdb.firebaseio.com",
+
+  projectId: "discordst-20a88",
+
+  storageBucket: "discordst-20a88.appspot.com",
+
+  messagingSenderId: "844700200303",
+
+  appId: "1:844700200303:web:a08306584112bbbaa98c9a",
+
+  measurementId: "G-54H6E5RCN6",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+// let's code 
+var datab  = firebase.database().ref('data');
+function UserRegister(){
+var email = document.getElementById('eemail').value;
+var password = document.getElementById('lpassword').value;
+firebase.auth().createUserWithEmailAndPassword(email,password).then(function(){
+    
+}).catch(function (error){
+    var errorcode = error.code;
+    var errormsg = error.message;
+});
+}
+const auth = firebase.auth();
+function SignIn(){
+    var email = document.getElementById('eemail').value;
+    var password = document.getElementById('lpassword').value;
+    const promise = auth.signInWithEmailAndPassword(email,password);
+    promise.catch( e => alert(e.msg));
+    window.open("https://www.google.com","_self");
+}
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    var userInfo = datab.push();
+    userInfo.set({
+        name: getId('fname'),
+        email : getId('eemail'),
+        password : getId('lpassword')
+    });
+    alert("Successfully Signed Up");
+    console.log("sent");
+    document.getElementById('form').reset();
+});
+function  getId(id){
+    return document.getElementById(id).value;
+}
